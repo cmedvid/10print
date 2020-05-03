cc8416772ef6567bfe10d18a9666e75276855e4e
cmedvid <catherine.medvid@gmail.com>
2020-05-03 15:25:27 -0400

    first draft -- planning to add color and asymmetric triangle design

--- 10print.js
+++ 10print.js
@@ -1,20 +1,52 @@
 /*
   one possible implemenation of the 10print.org algorithm written in JavaScript
+  EDITED by catie medvid
 */
 
+// some nice constants
 const w = process.stdout.columns
+const halfW = Math.floor(w/2)
+const characters = ['┼', '├', '┬', '┤', '┴']
 
-function draw () {
-  setTimeout(draw, 1000)
+// random character generator for triangle fill
+function getChar() {
+  return characters[Math.floor(Math.random() * Math.floor(5))]
+}
+
+// format lines
+function drawLine(num) {
   let output = ''
   for (let i = 0; i < w; i++) {
-    if (Math.random() > 0.5) {
-      output += '\\'
-    } else {
-      output += '/'
+    if (i < (halfW - 1 - num) || i > (halfW + 1 + num) 
+        || i == (halfW + 1) || i == (halfW - 1)) {
+      output += ' '
+    }
+    else if (i == halfW) {
+      output += '╳' // play around some to find a divider u like
+    }
+    else {
+      output += getChar()
     }
   }
   console.log(output)
 }
 
-draw()
+// create a triangle-like figure
+function drawTriangles() {
+  let i = Math.floor(Math.random() * Math.floor(w/5))
+  for (i; i > .5; i = Math.floor(i/1.5)){
+    drawLine(i)
+  }
+  drawLine(0)
+  let j = Math.floor(Math.random() * Math.floor(2))
+  for (j; j > 0; j--){
+    drawLine(0)
+  }
+}
+
+function drawMyChildren() {
+  setTimeout(drawMyChildren, 100) 
+  drawTriangles()
+}
+
+drawMyChildren()
