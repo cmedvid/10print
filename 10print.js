be5f4d6670826778f8b4c04b57cbd61249daa3f6
cmedvid <catherine.medvid@gmail.com>
2020-05-08 14:40:05 -0400

    BETTER COLORS

--- 10print.js
+++ 10print.js
@@ -8,6 +8,7 @@ const w = process.stdout.columns
 const halfW = Math.floor(w/2)
 const characters = ['┼', '├', '┬', '┤', '┴']
 const color = ['\x1b[32m', '\x1b[33m']
+const intensity = ['\x1b[1m', '\x1b[2m']
 
 // random character generator for triangle fill
 function getChar() {
@@ -17,10 +18,19 @@ function getChar() {
 // random color generator for triangle fill
 function getColor() {
   if (Math.random() > 0.2) {
-    return color[0]
+    return color[0] // green
   }
   else {
-    return color[1]
+    return color[1] // yellow
+  }
+}
+
+function getIntensity() {
+  if (Math.random() < 0.3) {
+    return intensity[0] // bright
+  }
+  else {
+    return intensity[1] // dim
   }
 }
 
@@ -28,20 +38,24 @@ function getColor() {
 function drawLine(num) {
   let output = ''
   for (let i = 0; i < w; i++) {
+    output += '\x1b[40m' // background to black
     if (i < (halfW - 1 - num) || i > (halfW + 1 + num) 
         || i == (halfW + 1) || i == (halfW - 1)) {
       output += ' '
     }
     else if (i == halfW) {
-      output += '\x1b[30m'
+      output += '\x1b[37m\x1b[2m' // white
       output += '│' // play around some to find a divider u like
+      output += '\x1b[0m' // reset color settings
     }
     else {
+      output += getIntensity()
       output += getColor()
       output += getChar()
+      output += '\x1b[0m' // reset color settings
     }
   }
-  // output += '\x1b[40m'
+  output += '\x1b[0m' // reset color settings
   console.log(output)
 }
 
